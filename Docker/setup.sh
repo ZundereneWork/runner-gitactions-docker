6f737d1ed72ad1fcff8ec155e11f61be9eba63d6
hector21450 <hector21450@gmail.com>
2023-03-31 21:41:03 +0200

    update

--- Docker/setup.sh
+++ Docker/setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+
 # Configure the runner to connect to a GitHub organization
 TOKEN=$(curl -X POST -H "Authorization: Bearer  $2" \
     -H "Accept: application/vnd.github.v3+json" \
     https://api.github.com/orgs/$1/actions/runners/registration-token \
     | jq -r '.token')
     
-    
+
 ./config.sh remove --token $TOKEN
-./config.sh --url https://github.com/$1 --token $TOKEN --name $3 --labels docker  --replace
+./config.sh --url https://github.com/$ORG --token $TOKEN --name $NAME --labels docker  --replace
 ./run.sh
 
