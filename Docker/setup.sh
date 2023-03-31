cd8cae4b47a74a4c1411532370dc84a845810458
hector21450 <hector21450@gmail.com>
2023-03-31 20:52:11 +0200

    forzar update

--- Docker/setup.sh
+++ Docker/setup.sh
@@ -4,6 +4,7 @@ TOKEN=$(curl -X POST -H "Authorization: Bearer  $2" \
     -H "Accept: application/vnd.github.v3+json" \
     https://api.github.com/orgs/$1/actions/runners/registration-token \
     | jq -r '.token')
+    
 
 ./config.sh remove --token $TOKEN
 ./config.sh --url https://github.com/$1 --token $TOKEN --name $3 --labels docker  --replace
