30725111f62359cf93d0423dc09c25b97a76b3c4
hector21450 <hector21450@gmail.com>
2023-04-01 22:40:40 +0200

    update delete show key

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-echo $ORG
-echo $TOKEN
-echo $NAME
 # Configure the runner to connect to a GitHub organization
 T=$(curl -X POST \
   "https://api.github.com/orgs/$ORG/actions/runners/registration-token" \
   --header 'User-Agent: Thunder Client (https://www.thunderclient.com)' \
   --header "Authorization: Bearer $TOKEN " | jq -r '.token')
-echo $T
 ./config.sh --url https://github.com/${ORG} --token ${T} --name ${NAME} --labels docker  --replace
 ./run.sh
